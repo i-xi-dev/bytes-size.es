7e1cea81c89a82410abbf44d3588b37defbb2f8a
i-xi-dev <naoyuki.isono@gmail.com>
2023-09-11 23:17:47 +0900

    1.0.7

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export {
   Integer,
   NonNegativeInteger,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.0/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.1/mod.ts";
